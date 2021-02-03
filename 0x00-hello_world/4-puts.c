0f6e7ae0427a157e8d4167502fd028ffa749ffd7
ElOchi <dantezell78@gmail.com>
2021-02-03 20:35:16 +0000

    fix exercise 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - puts line to stdout
- * Description: uses puts to output fixed message
- * Return: void
+ * main - runs the all the code
+ *
+ * Description: Run puts and print statement
+ * Return: Return 0 to exit function
  */
+
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
