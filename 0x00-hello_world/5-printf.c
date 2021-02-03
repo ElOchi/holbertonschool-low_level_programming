7e8564a8afd5f378f383441d3d5ef78555d05b8b
ElOchi <dantezell78@gmail.com>
2021-02-03 20:40:01 +0000

    add exercise 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - runs the all the code
+ *
+ * Description: Run printf and print statement
+ * Return: Return 0 to exit function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
