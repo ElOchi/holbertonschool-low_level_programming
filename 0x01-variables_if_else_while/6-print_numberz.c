4df2bbc34ddf74631916463469c051100593f094
ElOchi <dantezell78@gmail.com>
2021-02-04 23:23:23 +0000

    add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Prints number from 0 to 9.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar (num + '0');
+		num++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
