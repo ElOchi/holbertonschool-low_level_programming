7b1459b855aacc18ca5218c1d673d91ce563a826
ElOchi <dantezell78@gmail.com>
2021-02-05 01:15:47 +0000

    add 10-print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - Printsthe numbers from 0 to 99 separated by commas, followed by space
+ *
+ *
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	int nums = '0';
+	int secondNum = '0';
+
+	while (nums <= '9')
+	{
+		secondNum = '0';
+		while (secondNum <= '9')
+		{
+			putchar(nums);
+			putchar(secondNum);
+			if (!((nums == '9') && (secondNum == '9')))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			secondNum++;
+		}
+			nums++;
+	}
+
+	putchar('\n');
+	return (0);
+}
