1154a0581541fd5e1d91bc757aa205b6b120097d
ElOchi <dantezell78@gmail.com>
2021-02-08 19:06:59 +0000

    add 0-holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "holberton.h"
+/**
+* main -  prints Holberton, followed by a new line.
+* Return: 0 on success.
+*/
+
+
+
+int main(void)
+{
+char holbi[20] = "Holberton";
+
+	printf("%s\n", holbi);
+
+	return (0);
+}
+
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,20 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include<stdio.h>
 
-int main(void)
-{
-	int a = 0, b;
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
 
-	while (a < 5)
-	{
-		b = 0;
-		while (b < 4)
-		{
-			printf("3 ");
-			b++;
-		}
-
-		printf("\n");
-		a++;
-	}
-	return (0);
-}
