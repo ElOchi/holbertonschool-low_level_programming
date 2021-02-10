c1726f42ea9cf5a8561036c3f9cbdbeeb3fa7753
ElOchi <dantezell78@gmail.com>
2021-02-10 01:48:45 +0000

    add 102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int f1 = 1;
+	long int f2 = 2;
+	int count = 2;
+	long int fnext;
+
+	fnext = f1 + f2;
+	printf("1, 2, ");
+
+	while (count < 50)
+	{
+		printf("%li", fnext);
+		if (count != 49)
+		{
+			printf(", ");
+		}
+		f1 = f2;
+		f2 = fnext;
+		fnext = f1 + f2;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
+
