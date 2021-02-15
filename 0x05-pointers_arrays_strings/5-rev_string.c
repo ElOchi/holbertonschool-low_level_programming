4b12c365f146e8bb4e777801a04a5ff62923cb62
ElOchi <dantezell78@gmail.com>
2021-02-15 14:47:52 +0000

    fix 5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,13 @@ void rev_string(char *s)
 	int len = 0, end = 0;
 	char ch;
 
-   while (s[end++])
-	   len++;
-   for (end = len - 1; end >= len / 2; end--)
-   {
-	   ch = s[end];
-	   s[end] = s[len - end - 1];
-	  s[len - end - 1] = ch;
-   }
+	while (s[end++])
+		len++;
 
+	for (end = len - 1; end >= len / 2; end--)
+	{
+		ch = s[end];
+		s[end] = s[len - end - 1];
+		s[len - end - 1] = ch;
+	}
 }
