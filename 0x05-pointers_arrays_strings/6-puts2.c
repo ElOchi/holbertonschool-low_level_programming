aead21880aae8e20838a821f3701e7001b5d935b
ElOchi <dantezell78@gmail.com>
2021-02-15 14:50:56 +0000

    add 6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with first char
+ * @str: The string containing characters.
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
