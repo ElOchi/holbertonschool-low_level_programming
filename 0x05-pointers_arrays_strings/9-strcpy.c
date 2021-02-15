7132a22877602e40bcb132499cfc232f390063a8
ElOchi <dantezell78@gmail.com>
2021-02-15 18:57:21 +0000

    add 7-9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Copies string pointed to by src, including terminating null byte
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
