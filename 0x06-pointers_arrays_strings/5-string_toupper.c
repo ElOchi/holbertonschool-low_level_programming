f3f94b36272d600eddb58eff81c227bbfad751a5
ElOchi <dantezell78@gmail.com>
2021-02-19 00:52:43 +0000

    add 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Change lowercase letters of string to uppercase.
+ *
+ * @str: string to be changed.
+ *
+ * Return: pointer to changed string.
+ */
+
+
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
