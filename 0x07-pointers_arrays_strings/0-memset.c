659c77ff831c87eb07b48ba70f62e3b125ecfc65
ElOchi <dantezell78@gmail.com>
2021-02-22 20:10:15 +0000

    add 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include"holberton.h"
+
+/**
+ * _memset - Sets some elments in memory
+ * @s: memory area to set, the Array
+ * @b: the value we set to the memory area
+ * @n: the number of elements in the array to fill
+ *
+ * Return: Array with the modified elements
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
+
