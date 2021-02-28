ef544262cbafa9ce24a26d3e0d45bcccca378e72
ElOchi <dantezell78@gmail.com>
2021-02-28 20:11:38 +0000

    add 0 and 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include"holberton.h"
+/**
+ * main - Prints its name.
+ * @argc: number of arguments
+ * @argv: An array of pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
