56dc8cd2b72d617f712cdb170ab3ae1684c97e34
ElOchi <dantezell78@gmail.com>
2021-02-28 01:26:06 +0000

    add 0x09

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strcmp - Sets some elments in memory
+ * @s1: memory area to set, the Array
+ * @s2: the value we set to the memory area
+ *
+ * Return: Array with the modified elements
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (1);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strspn - Sets some elments in memory
+ * @s: memory area to set, the Array
+ * @accept: the value we set to the memory area
+ *
+ * Return: Array with the modified elements
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+return (1);
+}
