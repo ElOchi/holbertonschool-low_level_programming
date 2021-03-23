6cd745e16d7642fa59df6d94139ee2108d1f8348
ElOchi <dantezell78@gmail.com>
2021-03-23 16:07:02 +0000

    add

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,9 +11,12 @@ size_t print_listint(const listint_t *h)
 
 	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		print_listint(h->next);
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
-
 	return (count);
 }
