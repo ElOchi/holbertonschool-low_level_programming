7a76efcd62861200b5e6cff58630f6430d29ec57
ElOchi <dantezell78@gmail.com>
2021-03-23 17:24:42 +0000

    add

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,10 +10,10 @@ size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
-	while (h != NULL)
-	{
-		h = h->next;
-		count++;
-	}
+		while (h != NULL)
+		{
+			h = h->next;
+			count++;
+		}
 	return (count);
 }
