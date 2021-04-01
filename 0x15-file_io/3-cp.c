adb9db6588eb781b08baf2d2820b77a6c94d75df
ElOchi <dantezell78@gmail.com>
2021-04-01 16:23:24 +0000

    add

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,77 +1,55 @@
 #include "holberton.h"
+#ifndef BUFF_SIZE
+#define BUFF_SIZE 1024
+#endif
 
 /**
- * closefd - closes files
- * @fd: file descriptor
- * @buff: buffer
- * Return: value of file descriptor
+ * main - check the code for Holberton School students.
+ * @argc: name of my file
+ * @argv: number of the letters that i used
+ * Return: Always 0.
  */
 
-int closefd(int fd, char *buff)
+int main(int argc, char *argv[])
 {
-	int f;
+	int file_origin, file_dest;
+	ssize_t numb;
+	char buff[BUFF_SIZE];
 
-	f = close(fd);
-	if (f == -1)
+	if (argc != 3)
 	{
-		free(buff);
-		dprintf(2, "Error: Can't close fd %dn", f);
-		exit(100);
-	}
-	return (f);
-}
-
-/**
- * checkfail - checks fails
- * @fd: file descriptor
- * @check: check value
- * @str: string to print
- * @buff: buffer
- * @av: command
- * Return: fd
- */
-
-int checkfail(int fd, int check, char *str, char *buff, char *av)
-{
-	if (fd == -1 || check == -1)
-	{
-		free(buff);
-		dprintf(2, "Error: Can't %s %s\n", str, av);
-		exit(99);
-	}
-	return (fd);
-}
-
-/**
- * main - copies contents of one file to another
- * @ac: number of arguments
- * @av: array of arguments
- * Return: 1 on success and -1 on failure
- */
-
-int main(int ac, char **av)
-{
-	int fd1, fd2, f, check;
-	char *buff;
-
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = malloc(1024 * sizeof(char));
-	if (!buff)
-		return (-1);
-	for (f = 0; f < 1024; f++)
-		buff[f] = '\0';
-	fd1 = open(av[1], O_RDWR, 0664);
-	check = read(fd1, buff, 1024);
-	checkfail(fd1, check, "read", buff, av[2]);
-	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	check = write(fd2, buff, 1024);
-	checkfail(fd2, check, "write to", buff, av[2]);
-	closefd(fd1, buff);
-	closefd(fd2, buff);
-	free(buff);
-	return (1);
+	file_origin = open(argv[1], O_RDONLY);
+	if (file_origin == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	if (file_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
+	{
+		if (write(file_dest, buff, numb) != numb)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (numb == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if ((close(file_origin) == -1) || (close(file_dest) == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_origin);
+		exit(100);
+	}
+	exit(EXIT_SUCCESS);
 }
